8fb935b9b536d3d28ba6582fe277f235c8262001
OSmium <filip.ipod@googlemail.com>
2012-07-25 19:13:33 +0200

    neue test.java

--- TestProjekt/src/main/test.java
+++ TestProjekt/src/main/test.java
@@ -7,7 +7,8 @@ public class test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo github");
+		System.out.println("Hallo github gute sache");
+	
 	}
 
 }
